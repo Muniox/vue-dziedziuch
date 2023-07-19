214da5d024f49fb003c2bae47a14278714cc323e
Muniox <muniox20@gmail.com>
2023-07-19 13:14:01 +0200

    Added lesson 11

---
+++ 11.Sloty, czyli twórz bardziej uniwersalne komponenty/src/components/AwsomeButton.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    text: string
+  }>(),
+  {
+    text: 'siemka'
+  }
+)
+defineSlots<{
+  title: any
+  title2: any
+  default: any //slot defaultowy, bez nazwy
+}>()
+</script>
+
+<template>
+  <button class="button">
+    <slot>
+      {{ props.text }}
+      <slot name="title">
+        <h2>Earth</h2>
+      </slot>
+      <slot name="title2"></slot>
+    </slot>
+  </button>
+  <!-- <button class="button">
+    {{ props.text }}
+    <slot name="title">
+      <h2>Earth</h2>
+    </slot>
+    <slot name="title2"></slot>
+    <slot></slot>
+  </button> -->
+</template>
+
+<style scoped lang="scss">
+.button {
+  cursor: pointer;
+  background-color: transparent;
+  color: #000;
+  border: 3px solid #000;
+  padding: 10px 20px;
+  font-size: 24px;
+  outline: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.1s ease-in, color 0.1s ease-in, background-color 0.1s ease-in;
+  &:hover {
+    background-color: #000;
+    color: #fff;
+    background-color: #000;
+  }
+}
+</style>
---
+++ 11.Sloty, czyli twórz bardziej uniwersalne komponenty/src/components/MyAwesomeComponent.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import AwsomeButton from './AwsomeButton.vue'
+// skrót dla v-slot to #
+</script>
+
+<template>
+  <div class="container">
+    <AwsomeButton :text="'test'">
+      <template #title>
+        <h1>World</h1>
+      </template>
+      <template v-slot:title2>
+        <h1>World2</h1>
+      </template>
+      <p>Miłego dnia</p>
+    </AwsomeButton>
+    <!-- <AwsomeButton :text="'test'">
+      <template #title>
+        <h1>World</h1>
+      </template>
+      <template v-slot:title2>
+        <h1>World2</h1>
+      </template>
+      <p>Miłego dnia</p>
+    </AwsomeButton> -->
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
