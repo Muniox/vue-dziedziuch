13ba6d083ff87d36d57fc92b57dd2016b1ee557f
Muniox <muniox20@gmail.com>
2023-07-23 00:04:50 +0200

    Added episode 18

---
+++ 18.Vue router, czyli dodajemy do projektu obsługę routingu/src/components/MyAwesomeComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">Test</div>
+</template>
+
+<style lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  margin: 20px;
+}
+.header {
+  margin-bottom: 20px;
+}
+</style>
